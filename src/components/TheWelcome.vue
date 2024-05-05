8d4cf4dcd23323c90ffe21495305428ecc1bc929
tanny <tannyapriliani271@gmail.com>
2024-05-05 20:50:22 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- Element input teks -->
+    <input
+      type="text"
+      v-model="inputText"
+      @input="handleInput"
+      :class="{ 'input-error': isError }"
+      :style="{ backgroundColor: inputText === '' ? 'lightcoral' : 'transparent' }"
+      placeholder="Type something..."
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputText = ref('');
+const isError = ref(false);
+
+const handleInput = () => {
+  isError.value = inputText.value === '';
+}
+</script>
+
+<style scoped>
+.input-error {
+  border: 2px solid red;
+}
+</style>
